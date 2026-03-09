1e66e16ecdbda6ab7b2e814ef7210d7b42e94c90
Nathan <nathaniellucero03@gmail.com>
2026-03-09 09:36:56 +0800

    reinstalled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-
-
-
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
@@ -11,7 +10,6 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
       <Sonner />
       <BrowserRouter>
         <Routes>
